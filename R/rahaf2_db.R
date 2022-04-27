5791d9403844a2edeb127548a9e4d6079cbe7d44
Nimrod <nimrod.arch@gmail.com>
2022-04-27 23:25:26 +0300

    added .rda to data() files in /data

---
+++ R/rahaf2_db.R
@@ -0,0 +1,13 @@
+#' Faunal data base from Nahal Rahaf 2 (NR2)
+#'
+#' The site was excavated by Maayan Shemer, Nimrod Marom and Omry Barzilai in 2019-2020. The bones were identfied by Dariya Lokshin, archaeozoology Laboratory, Haifa. Part of the DEADSEA_ECO ERC Project. 
+#'
+#' @docType data
+#'
+#' @usage data(rahaf2_db)
+#'
+#' @keywords datasets
+#'
+#' @references in prep.
+
+"rahaf2_db"
---
+++ R/rahaf2_zl.R
@@ -0,0 +1,13 @@
+#' List of diagnostic zones used in the coding of the NR2 fauna
+#'
+#' The site was excavated by Maayan Shemer, Nimrod Marom and Omry Barzilai in 2019-2020. The bones were identfied by Dariya Lokshin, archaeozoology Laboratory, Haifa. Part of the DEADSEA_ECO ERC Project. 
+#'
+#' @docType data
+#'
+#' @usage data(rahaf2_zl)
+#'
+#' @keywords datasets
+#'
+#' @references in prep.
+
+"rahaf2_zl"
